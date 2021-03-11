14dda87c0173e3e090a0141daf3f05a2e49d9f6a
Lewis <121012648@qq.com>
2021-03-11 20:59:39 +0800

    springboot project is deployed to tomcat successfully

--- src/main/java/com/qikegu/demo/SpringbootMybatisDemoApplication.java
+++ src/main/java/com/qikegu/demo/SpringbootMybatisDemoApplication.java
@@ -3,10 +3,16 @@ package com.qikegu.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.qikegu.demo.repository")
-public class SpringbootMybatisDemoApplication {
+public class SpringbootMybatisDemoApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(this.getClass());
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Lewis demo2");
